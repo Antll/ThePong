5491cb52fa04103e393bcf9f6bd3836f9c48357c
Anatoliy <funnysage@gmail.com>
2018-11-15 16:07:41 +0800

    Insert debugger breakpoint when error occures

---
+++ headers/ErrorHandling.h
@@ -0,0 +1,22 @@
+#ifndef ERROR_HANDLING_H
+#define ERROR_HANDLING_H
+
+#include <GL/glew.h>
+#include <iostream>
+#include <signal.h>
+
+#ifdef SIGTRAP 
+    #define BREAK_POINT raise(SIGTRAP);
+#else 
+    #define BREAK_POINT raise(SIGABRT);
+#endif
+
+#define ASSERT(x) if (!(x)) BREAK_POINT
+#define GL_CHECK(x) GLClearError(); \
+    x;\
+    ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+
+void GLClearError();
+bool GLLogCall(const char* FunctionName, const char* SourceFileName, int Line);
+
+#endif // ERROR_HANDLING_H
