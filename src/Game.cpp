ba44bc5f77bacca898ceff8c27686155dbf9b6d2
Anatoliy <funnysage@gmail.com>
2018-11-20 22:07:58 +0800

    Free memory after destruction

--- src/Game.cpp
+++ src/Game.cpp
@@ -3,13 +3,17 @@
 UGame::UGame(UDisplay* Display)
 {
     Player = new UController(0.0f, -280.0f, 0.0f, 100.0f, 20.0f, Display);
+    TwinPlayer = new UController(0.0f, 280.0f, 0.0f, 100.0f, 20.0f, Display);
+    Ball = new UBall(0.0f, 0.0f, 0.0f, 30.0f, Display);
     return;
 }
 
 
 UGame::~UGame()
 {
-    Player = nullptr;
+    delete Player;
+    delete TwinPlayer;
+    delete Ball;
     return;
 }
 
@@ -17,6 +21,8 @@ UGame::~UGame()
 void UGame::Update()
 {
     Player->Update();
+    TwinPlayer->Update();
     Player->Draw();
+    TwinPlayer->Draw();
     return;
 }
